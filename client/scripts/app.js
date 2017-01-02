d60db92b00b0b12a724b38355d3b2756bc9f790d
AbiyMelaku <agirma08@gmail.com>
2017-01-02 15:27:03 -0800

    Added GET and POST methodsS

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,71 @@
 // YOUR CODE HERE:
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/messages', 
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
+var app = {
+  server: 'https://api.parse.com/1/classes/messages',
+
+  init: function() {
+
   },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
+
+  send: function(message) {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox:  Message sent');
+      },
+      error: function(data) {
+        console.log('Failed to send message', data);
+      }
+    });
+  },
+
+  fetch: function() {
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to retrieve message(s)', data);
+      }
+    });
   }
-});
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/messages', 
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
