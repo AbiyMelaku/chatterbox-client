47624c1c05c25e67f0f222ead25791da66f67775
AbiyMelaku <agirma08@gmail.com>
2017-01-02 16:50:33 -0800

    Add rooms, and messages built out

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -25,18 +25,51 @@ var app = {
     $.ajax({
       url: app.server,
       type: 'GET',
-      data: message,
+      data: message, //not sure about this, //{order -created at} ?????
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message received');
+        console.log('chatterbox: Message received', data); 
       },
       error: function (data) {
         console.error('chatterbox: Failed to retrieve message(s)', data);
       }
     });
+  },
+
+  clearMessages: function() {
+    $('#chats').html('');
+  },
+
+  renderMessage: function(message) {
+    var $chat = $('<div class="chat"></div>'); 
+    var $username = $('<span class="username"/>');
+    var $message = $('<br><span/>');
+    
+    $username.text(message.username).appendTo($chat);
+    $message.text(message.text).appendTo($chat);
+
+    $('#chats').append($chat);
+
+
+  },
+
+  renderRoom: function(roomname) {
+    var $option = $('<option/>').val(roomname).text(roomname);
+    $('#roomSelect').append($option);
+  },
+
+  handleUsernameClick: function () {
+
   }
 
 };
+
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
 
 
 
